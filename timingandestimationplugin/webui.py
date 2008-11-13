6912e925fa43a049e0ef4a360ff603369c7a5251
bobbysmith007 <bobbysmith007@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-11-13 00:31:50 +0000

    closes #4008 Made Billing page matching more precise
    git-svn-id: http://trac-hacks.org/svn/timingandestimationplugin/branches/trac0.11@4782 7322e99d-02ea-0310-aa39-e9a107903beb

--- timingandestimationplugin/webui.py
+++ timingandestimationplugin/webui.py
@@ -45,7 +45,8 @@ class TimingEstimationAndBillingPage(Component):
 
     # INavigationContributor methods
     def get_active_navigation_item(self, req):
-        if re.search('/Billing', req.path_info):
+        val = re.search('/Billing$', req.path_info)
+        if val and val.start() == 0:
             return "Billing"
         else:
             return ""
@@ -73,7 +74,8 @@ class TimingEstimationAndBillingPage(Component):
         data['billing_info']["billdates"] = billing_dates
 
     def match_request(self, req):
-        return req.path_info.startswith('/Billing')
+        val = re.search('/Billing$', req.path_info)
+        return val and val.start() == 0
 
     def process_request(self, req):
         messages = []
