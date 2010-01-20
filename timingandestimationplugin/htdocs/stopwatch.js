9791411b669a38da601f65f65ea087b6683d6f57
Tay Ray Chuan <rctay89@gmail.com>
2010-01-20 17:42:38 +0800

    insert placeholders

--- timingandestimationplugin/htdocs/stopwatch.js
+++ timingandestimationplugin/htdocs/stopwatch.js
@@ -1,3 +1,16 @@
 /**
  * Copyright (C) 2010, Tay Ray Chuan
  */
+
+$(document).ready(function() {
+	var toggler, stopwatch;
+	
+	stopwatch = $('<div>stopwatch goes here</div>');
+	toggler = $('<div>Show stopwatch</div>')
+		.append(stopwatch);
+	toggler.click(function() {
+		stopwatch.toggle("fast");
+	});
+	
+	$("input#field-hours").after(toggler);
+})
