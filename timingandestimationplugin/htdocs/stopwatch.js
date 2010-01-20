a9a92a423429b9bff21fb3a75787f2db6a82df0d
Tay Ray Chuan <rctay89@gmail.com>
2010-01-20 18:15:05 +0800

    comment stuff

--- timingandestimationplugin/htdocs/stopwatch.js
+++ timingandestimationplugin/htdocs/stopwatch.js
@@ -5,6 +5,7 @@
 $(document).ready(function() {
 	var toggler, stopwatch;
 
+	/* the stopwatch (looks like 00:00:00) */
 	var field_hour = $('<span></span>');
 	var field_min = $('<span></span>');
 	var field_sec = $('<span></span>');
@@ -16,11 +17,14 @@ $(document).ready(function() {
 		.append(':')
 		.append(field_sec);
 
+	/* toggles the stopwatch with a simple slide */
 	toggler = $('<div>Show stopwatch</div>')
 	toggler.click(function() {
 		stopwatch.toggle("fast");
 	});
 
+	/* put toggler and stopwatch in a div, then put it below the hours <input>
+	 * field. */
 	$("input#field-hours").after(
 		$('<div></div>')
 		.append(toggler)
