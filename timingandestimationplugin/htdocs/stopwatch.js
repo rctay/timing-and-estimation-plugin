6a41bbea6522acf8a51cab0074d8c5841b1d4f18
Tay Ray Chuan <rctay89@gmail.com>
2010-01-20 19:00:39 +0800

    add controls for stopwatch

--- timingandestimationplugin/htdocs/stopwatch.js
+++ timingandestimationplugin/htdocs/stopwatch.js
@@ -17,7 +17,48 @@ $(document).ready(function() {
 		.append(':')
 		.append(field_sec);
 
-	/* toggles the stopwatch with a simple slide */
+	/*
+ 	 * add buttons to control the stopwatch
+	 *
+	 * Button 'flow' has states: start -> stop <-> continue
+	 * Button 'reset' has state: reset
+	 */
+	var btn_flow = $('<div style="float: left"></div>');
+	var btn_reset = $('<div style="float: left">Reset</div>');
+	stopwatch.append($('<div></div>')
+		.append(btn_flow)
+		.append(btn_reset));
+
+	var running = 0;
+	var pause_stopwatch = function() {
+		btn_flow.text('Continue');
+		btn_reset.show();
+	};
+	var continue_stopwatch = function() {
+		btn_flow.text('Pause');
+		btn_reset.hide();
+	};
+	var reset_stopwatch = function() {
+		btn_flow.text('Start');
+		btn_reset.hide();
+	};
+
+	btn_flow.click(function() {
+		if (running)
+			pause_stopwatch();
+		else
+			continue_stopwatch();
+		running = !running;
+	});
+
+	btn_reset.click(function() {
+		if (running) return;
+
+		reset_stopwatch();
+		running = false;
+	});
+
+	/* toggles the stopwatch (and controls) with a simple slide */
 	toggler = $('<div>Show stopwatch</div>')
 	toggler.click(function() {
 		stopwatch.toggle("fast");
