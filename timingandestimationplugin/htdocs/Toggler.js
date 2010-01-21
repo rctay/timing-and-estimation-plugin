7fced4f41f6a1fb8cbc356a854f9b7c094579f4d
Tay Ray Chuan <rctay89@gmail.com>
2010-01-21 19:14:58 +0800

    put namespaces into closures called on ready

--- timingandestimationplugin/htdocs/Toggler.js
+++ timingandestimationplugin/htdocs/Toggler.js
@@ -3,58 +3,60 @@
  */
 
 /* toggles the stopwatch (and controls) with a simple slide */
-Toggler = function() {
-	var m_state;
-	var m_field;
+$(function() {
+	Toggler = function() {
+		var m_state;
+		var m_field;
 
-	var should_show = true;
-	var toggler = $('<div>Show stopwatch</div>');
-	toggler.click(function() {
-		if (should_show = !should_show) {
-			$(this).trigger("show");
-		} else
-			$(this).trigger("hide");
-	});
+		var should_show = true;
+		var toggler = $('<div>Show stopwatch</div>');
+		toggler.click(function() {
+			if (should_show = !should_show) {
+				$(this).trigger("show");
+			} else
+				$(this).trigger("hide");
+		});
 
-	toggler.bind("show",
-		function() {
-			if (m_state.running)
-				return false;
-			if (m_state.use_value) {
-				m_field.value = StopwatchDisplay.get_hours();
+		toggler.bind("show",
+			function() {
+				if (m_state.running)
+					return false;
+				if (m_state.use_value) {
+					m_field.value = StopwatchDisplay.get_hours();
+				}
+				this.firstChild.nodeValue = 'Show stopwatch';
 			}
-			this.firstChild.nodeValue = 'Show stopwatch';
-		}
-	);
-	toggler.bind("hide",
-		function() {
-			if (m_state.reset)
-				this.firstChild.nodeValue = 'Hide stopwatch';
-			else
-				this.firstChild.nodeValue = 'Use stopwatch value';
-		}
-	);
-	var btn_flow_click = function() {
-		if (m_state.running) {
-			toggler.hide("fast");
-		} else {
-			toggler[0].firstChild.nodeValue = 'Use stopwatch value';
-			toggler.show("fast");
-		}
-	};
-	var btn_reset_click = function() {
-		if (m_state.reset && !m_state.running)
-			toggler[0].firstChild.nodeValue = 'Hide stopwatch';
-	};
+		);
+		toggler.bind("hide",
+			function() {
+				if (m_state.reset)
+					this.firstChild.nodeValue = 'Hide stopwatch';
+				else
+					this.firstChild.nodeValue = 'Use stopwatch value';
+			}
+		);
+		var btn_flow_click = function() {
+			if (m_state.running) {
+				toggler.hide("fast");
+			} else {
+				toggler[0].firstChild.nodeValue = 'Use stopwatch value';
+				toggler.show("fast");
+			}
+		};
+		var btn_reset_click = function() {
+			if (m_state.reset && !m_state.running)
+				toggler[0].firstChild.nodeValue = 'Hide stopwatch';
+		};
 
-	return {
-		toggler: toggler,
-		btn_flow_click: btn_flow_click,
-		btn_reset_click: btn_reset_click,
+		return {
+			toggler: toggler,
+			btn_flow_click: btn_flow_click,
+			btn_reset_click: btn_reset_click,
 
-		init: function(state, field) {
-			m_state = state;
-			m_field = field;
-		}
-	};
-}();
+			init: function(state, field) {
+				m_state = state;
+				m_field = field;
+			}
+		};
+	}();
+});
