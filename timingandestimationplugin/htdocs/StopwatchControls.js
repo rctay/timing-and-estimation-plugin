74f2bc7fd533c250cabcd93101f437109f0aae74
Tay Ray Chuan <rctay89@gmail.com>
2010-01-22 14:17:56 +0800

    bind button state changes separately
    This allows one to trigger events on StopwatchControls.controls, still
    causing button states to change.

--- timingandestimationplugin/htdocs/StopwatchControls.js
+++ timingandestimationplugin/htdocs/StopwatchControls.js
@@ -11,32 +11,50 @@ jQuery(function($) {
 			.append(btn_flow)
 			.append(btn_reset);
 
-		btn_flow.click(function() {
-			if (m_state.running) {
-				controls.trigger('pause');
-
-				btn_flow.text('Continue');
-				btn_reset.show();
-			} else {
-				controls.trigger('continue');
-
-				btn_flow.text('Pause');
-				btn_reset.hide();
-			}
-			m_state.running = !m_state.running;
-			m_state.use_value = !m_state.running;
+		controls.bind('pause', function() {
+			m_state.running = false;
+			m_state.use_value = true;
 			m_state.reset = false;
+		});
+
+		controls.bind('continue', function() {
+			m_state.running = true;
+			m_state.use_value = false;
+			m_state.reset = false;
+		});
+
+		controls.bind('reset', function() {
+			m_state.running = false;
+			m_state.use_value = false;
+			m_state.reset = true;
+		});
+
+		btn_flow.click(function() {
+			if (m_state.running)
+				controls.trigger('pause');
+			else
+				controls.trigger('continue');
 		});
 
 		btn_reset.click(function() {
 			if (m_state.running) return;
 
 			controls.trigger('reset');
+		});
 
+		controls.bind('pause', function() {
+			btn_flow.text('Continue');
+			btn_reset.show();
+		});
+
+		controls.bind('continue', function() {
+			btn_flow.text('Pause');
+			btn_reset.hide();
+		});
+
+		controls.bind('reset', function() {
 			btn_flow.text('Start');
 			btn_reset.hide();
-			m_state.running = false;
-			m_state.reset = true;
 		});
 
 		return {
