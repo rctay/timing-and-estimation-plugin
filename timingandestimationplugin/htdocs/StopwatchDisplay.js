1f724ddc1c7e067c2b716bfa414ed5382d874633
Tay Ray Chuan <rctay89@gmail.com>
2010-01-27 09:05:58 +0800

    convert spaces to tabs

--- timingandestimationplugin/htdocs/StopwatchDisplay.js
+++ timingandestimationplugin/htdocs/StopwatchDisplay.js
@@ -28,18 +28,18 @@ jQuery(function($) {
 
 		var interval_id;
 		var start_time = null;
-	        var end_time;
-	        var total_time=0 ;
-	        var now = function(){ return Math.floor((new Date()).getTime() / 1000);};
+		var end_time;
+		var total_time=0 ;
+		var now = function(){ return Math.floor((new Date()).getTime() / 1000);};
 		var interval_func = function() {
-                        var interval = (now() - start_time)+total_time;
-                        var h = 0, m = 0, s = 0;
-                        s = interval % 60;
-                        m = Math.floor(interval/60) % 60;
-                        h = Math.floor(interval/3600);
-                        field_sec.nodeValue = s < 10 ? '0'+s : s;
-                        field_min.nodeValue = m < 10 ? '0'+m : m;
-                        field_hour.nodeValue = h < 10 ? '0'+h : h;
+			var interval = (now() - start_time)+total_time;
+			var h = 0, m = 0, s = 0;
+			s = interval % 60;
+			m = Math.floor(interval/60) % 60;
+			h = Math.floor(interval/3600);
+			field_sec.nodeValue = s < 10 ? '0'+s : s;
+			field_min.nodeValue = m < 10 ? '0'+m : m;
+			field_hour.nodeValue = h < 10 ? '0'+h : h;
 		};
 
 		return {
@@ -48,28 +48,28 @@ jQuery(function($) {
 			pause_stopwatch: function() {
 				clearInterval(interval_id);
 				interval_id = null;
-			        total_time += now() - start_time;
-			        start_time = null;
+				total_time += now() - start_time;
+				start_time = null;
 			},
 			continue_stopwatch: function() {
 				/*
 				 * We really want to do an add (of the seconds on the display)
 				 * - which is what we get when start_time is subtracted later.
 				 */
-			        start_time = now();
+				start_time = now();
 				interval_id = setInterval(interval_func, 100);
 			},
 			reset_stopwatch: function() {
 				field_hour.nodeValue = '00';
 				field_min.nodeValue = '00';
 				field_sec.nodeValue = '00';
-                                start_time=null;
-                                total_time=0;
+				start_time=null;
+				total_time=0;
 			},
 			get_hours: function() {
-                                var total = total_time;
-			        if (start_time) total += (now() - start_time);
-                                return Math.round( ( total / 3600 ) * 100) / 100;
+				var total = total_time;
+				if (start_time) total += (now() - start_time);
+				return Math.round( ( total / 3600 ) * 100) / 100;
 			}
 		};
 	}();
